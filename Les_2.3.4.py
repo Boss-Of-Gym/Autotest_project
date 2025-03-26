c09e734e44878803d6a7e0ef258cc39c8d2eb88b
Boss of gym <naydanov.dmitriy@gmail.com>
2025-03-26 13:25:06 +0200

    Коды второго блока

---
+++ Les_2.3.4.py
@@ -0,0 +1,30 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import math
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+try: 
+    link = "http://suninjuly.github.io/alert_accept.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    button = browser.find_element(By.CSS_SELECTOR, 'button.btn')
+    button.click()
+    confirm = browser.switch_to.alert
+    confirm.accept()
+
+    x_element = browser.find_element(By.ID, 'input_value')
+    x = x_element.text
+    y = calc(x)
+    answer_les = browser.find_element(By.ID, 'answer')
+    answer_les.send_keys(y)
+
+    button_math = browser.find_element(By.CSS_SELECTOR, 'button.btn')
+    button_math.click()
+
+finally:
+    time.sleep(20)
+    browser.quit()
