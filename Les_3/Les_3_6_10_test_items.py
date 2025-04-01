8e2a9fa9467024f77d5175bd6b3f789ee27ebac4
Boss-Of-Gym <naydanov.dmitriy@gmail.com>
2025-04-01 11:55:29 +0200

    script for check

--- Les_3/Les_3_6_10_test_items.py
+++ Les_3/Les_3_6_10_test_items.py
@@ -1,7 +1,11 @@
+import time
 from selenium.webdriver.common.by import By
 
 def test_add_to_cart_button_exists(browser):
     url = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(url)
+    time.sleep(30)
     add_to_cart_button = browser.find_elements(By.CSS_SELECTOR, ".btn.btn-lg.btn-primary.btn-add-to-basket")
     assert len(add_to_cart_button) > 0, "Кнопка 'Добавить в корзину' не найдена на странице"
+    button_text = add_to_cart_button[0].text
+    print(f"Button text: {button_text}")
